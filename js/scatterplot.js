176101789e029d4213f6054670ad1cd0eecd62c4
Caitlin <caitlin.c.chao@gmail.com>
2022-02-28 13:40:18 -0500

    could not do python server but started scatterplot

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -7,8 +7,17 @@ Modified: 12/08/21
 */
 
 // Build your scatterplot in this file 
+const widthScp = 1000;
+const heightScp = 900;
+const marginScp = {top: 50, bottom: 50, left: 50, right: 50}; 
+const tooltip = 15;
 
-
+const csvSCP = d3
+.select("#csv-scatter")
+.append("svg")
+.attr("height", heightScp - marginScp.top - marginScp.bottom)
+.attr("width", widthScp-marginScp.left-marginScp.right)
+.attr("viewBox", [0, 0, widthScp, heightScp]);
 
 
 
